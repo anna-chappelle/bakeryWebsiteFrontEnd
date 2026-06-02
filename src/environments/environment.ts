686635659aa41ffd3c812f455ed1b043a197cc06
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-06-01 20:40:36 -0400

    update backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: "https://website-backend-production-0aee.up.railway.app:8080"
+  backendUrl: "https://website-backend-production-0aee.up.railway.app"
 };
