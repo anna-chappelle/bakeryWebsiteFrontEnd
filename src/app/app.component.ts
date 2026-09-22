263cf7fb68e501ec253864ea3c525c085f72053e
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-09-22 18:31:36 -0400

    clear init code from app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BackendService } from '@services/backend.service';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +9,4 @@ import { tap } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  private readonly backendService = inject(BackendService);
-  protected readonly title = 'bakery-website-frontend';
-
-  protected backendResponse = signal<string>('~loading~');
-
-  ngOnInit(): void {
-    this.backendService
-      .testBackend()
-      .pipe(
-        tap((res) => {
-          console.dir(res);
-          return this.backendResponse.set(JSON.stringify(res));
-        }),
-      )
-      .subscribe();
-  }
-}
+export class AppComponent {}
