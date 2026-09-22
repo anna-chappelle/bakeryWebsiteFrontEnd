7ded02baf93e5e87a764682ea4816d66198d99d4
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-09-22 18:33:31 -0400

    add header comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
